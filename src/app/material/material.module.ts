3631b49af6d8d20c6bb818990d76b6b00bc66305
Marc Girbau <marc.girbau94@gmail.com>
2023-06-28 23:47:08 +0200

    Import fonts and material icons in styles file

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from "@angular/core";
+import { MatIconModule } from "@angular/material/icon";
 import { MatInputModule } from "@angular/material/input";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
-  exports: [MatInputModule],
+  exports: [MatIconModule, MatFormFieldModule, MatInputModule, FormsModule],
 })
 export class MaterialModule {}
