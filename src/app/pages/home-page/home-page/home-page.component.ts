48b375189cd269b2725df67281ba1edf41be3d85
Marc Girbau <marc.girbau94@gmail.com>
2023-06-29 11:40:24 +0200

    Implement searchMovies method to HomePageComponent

--- src/app/pages/home-page/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page/home-page.component.ts
@@ -1,8 +1,22 @@
-import { Component } from "@angular/core";
+import { Component, Inject } from "@angular/core";
+import { type Observable } from "rxjs";
+import { MoviesService } from "src/app/services/movies/movies.service";
+import { type ParsedMoviesApiResponse, type Movies } from "src/types/types";
 
 @Component({
   selector: "app-home-page",
   templateUrl: "./home-page.component.html",
   styleUrls: ["./home-page.component.scss"],
 })
-export class HomePageComponent {}
+export class HomePageComponent {
+  searchValue: string;
+  movies$: Observable<ParsedMoviesApiResponse>;
+
+  constructor(
+    @Inject(MoviesService) private readonly moviesService: MoviesService
+  ) {}
+
+  searchMovies(title: string): void {
+    this.movies$ = this.moviesService.searchMovies(title);
+  }
+}
