7effadb6a778579e288f31183671ee0909c38b02
Marc Girbau <marc.girbau94@gmail.com>
2023-06-30 01:31:49 +0200

    Create deleteFromFavourites action

--- src/app/store/movies/movies.actions.ts
+++ src/app/store/movies/movies.actions.ts
@@ -5,3 +5,8 @@ export const addToFavourites = createAction(
   "[Movies] Add Movie",
   props<{ payload: FavMovie }>()
 );
+
+export const deleteFromFavourites = createAction(
+  "[Movies] Delete Movie",
+  props<{ payload: string }>()
+);
