ff39378211dc523a155813c7e39feae0485f179e
Marc Girbau <marc.girbau94@gmail.com>
2023-06-29 22:21:54 +0200

    Implement addToFavourites moviesReducer case

--- src/app/store/movies/movies.reducer.ts
+++ src/app/store/movies/movies.reducer.ts
@@ -1,11 +1,18 @@
 import { createFeature, createReducer, on } from "@ngrx/store";
 import { type FavouriteMovies } from "./types";
+import { addToFavourites } from "./movies.actions";
 
 export const initialState: FavouriteMovies = [];
 
 export const moviesFeature = createFeature({
   name: "movies",
-  reducer: createReducer(initialState),
+  reducer: createReducer(
+    initialState,
+    on(
+      addToFavourites,
+      (currentState, { payload }): FavouriteMovies => [...currentState, payload]
+    )
+  ),
 });
 
 export const {
